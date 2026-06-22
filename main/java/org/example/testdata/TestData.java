55aa7dd273d9a61a120c5e5fa6cc6f62a6b3b5da
tarunbishtt <tarun.bisht@zohomail.in>
2026-06-22 18:17:50 +0530

    Add files via upload

--- main/java/org/example/testdata/TestData.java
+++ main/java/org/example/testdata/TestData.java
@@ -1,5 +1,9 @@
 package org.example.testdata;
 
+import org.example.utils.ExcelUtils;
+import org.testng.annotations.DataProvider;
+import java.io.IOException;
+
 public class TestData {
 
     //Valid credentials
@@ -13,6 +17,26 @@ public class TestData {
     //BaseURL
     public static final String base_Url = "https://www.saucedemo.com";
 
+    public static final String excel_path = System.getProperty("user.dir") + "//testData.xlsx";
+    @DataProvider(name = "login_data")
+    public Object [][] getLoginData() throws IOException{
+        ExcelUtils excel = new ExcelUtils(excel_path, "testData");
+        int totalRows = excel.getRowCount();
+        int totalCols = excel.getColumnCount();
+
+        System.out.println("Total rows: " + totalRows);
+        System.out.println("Total cols: " + totalCols);
+
+        Object [][] data = new Object[totalRows][totalCols];
+
+        for (int i = 1; i <= totalRows; i++){
+            for(int j = 0; j < totalCols; j++){
+                data [i-1] [j] = excel.getCellData(i, j);
+            }
+        }
+        excel.closeWorkbook();
+        return data;
+    }
     //Product
     public static final String product = "Sauce Labs Backpack";
 
